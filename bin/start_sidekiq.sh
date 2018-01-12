e23e4f9f41b492369d38c903e63f004f7664ad7d
Gildo Fiorito <fiorito.g@gmail.com>
2018-01-12 16:51:30 +0100

    fix sidekiq run

--- bin/start_sidekiq.sh
+++ bin/start_sidekiq.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sidekiq -r /home/gildo/rocco/bin/bot.rb -C /home/gildo/rocco/config/sidekiq.yml
+bundle exec sidekiq -r /home/gildo/rocco/bin/bot.rb -C /home/gildo/rocco/config/sidekiq.yml
