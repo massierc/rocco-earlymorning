55f120cf1e1893eee75900d1005525b80bf1a562
Gildo Fiorito <fiorito.g@gmail.com>
2018-03-01 13:06:28 +0100

    A2A special case

--- app/jobs/riccardo_job.rb
+++ app/jobs/riccardo_job.rb
@@ -11,9 +11,15 @@ class RiccardoJob < ApplicationJob
     User.find_by_username(user).uid
   end
 
+  def super_sheet
+    if Rails.env.production?
+      '1RTNQUTh3oLnT1UL7rJgMoQAz5Uuhth99hAYtxWjOKvI'
+    else
+      '1g6Rn0cH_u4ViLDLjlnJDEKcT3rPt-7EaveDbdOtK1TY'
+    end
+  end
   def perform(*_args)
     today = Date.today
-    super_sheet = '1RTNQUTh3oLnT1UL7rJgMoQAz5Uuhth99hAYtxWjOKvI'
     service = Authorizer.new(riccardo_uid).service
     sheets = service.get_spreadsheet(super_sheet).sheets.collect { |x| x.properties.title }
 
@@ -35,17 +41,50 @@ class RiccardoJob < ApplicationJob
 
       current_name = nil
 
+
+      a2a_cells = {}
+
       sheets.each do |s|
         projects.each_with_index do |p, i|
           if !p.empty? && p[1].downcase.strip.include?(s.downcase.strip)
             current_name = p[0].capitalize
-            cells[s] += p[-1].to_i
+
+            # A2A Case
+            if p[1] =~ /A2A/i
+              a2a_cells[p[2]] = p[-1].to_i
+              cells[s] = a2a_cells
+            else
+              cells[s] += p[-1].to_i
+            end
+
           end
         end
       end
 
+
       cells.each do |k,v|
-        if v > 0
+        if v.kind_of?(Hash)
+          # A2A Case!
+          v.each do |activity,vv|
+            svalues = service.get_spreadsheet_values(super_sheet, "#{k}!D:G").values
+            range = svalues.index([today.year.to_s, today.month.to_s, current_name, activity])
+            if range
+              range += 1
+            else
+              range = svalues.length + 1
+            end
+
+            val = [[today.year.to_s, today.month.to_s, current_name, "#{activity} #{today.strftime('%b')}",
+              nil, nil,
+              vv / 8.0,
+              nil, nil, nil, nil,
+              vv / 8.0]]
+
+            service.update_spreadsheet_value(super_sheet, "#{k}!D#{range}", values(val), value_input_option: 'USER_ENTERED')
+
+          end
+
+        elsif v > 0
           svalues = service.get_spreadsheet_values(super_sheet, "#{k}!D:F").values
           range = svalues.index([today.year.to_s, today.month.to_s, current_name])
           if range
