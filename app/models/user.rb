5d3cde4df1eb3f0198f7ece4d826854f8f8d0f64
Massimo Ercolani <massimo.ercolani28@gmail.com>
2018-07-23 14:57:28 +0200

    dependent destroy on user-workday and user-worksession association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :work_sessions
-  has_many :work_days
+  has_many :work_sessions, dependent: :destroy
+  has_many :work_days, dependent: :destroy
 
   def active_worksession
     self.work_sessions.find_by_end_date(nil)
