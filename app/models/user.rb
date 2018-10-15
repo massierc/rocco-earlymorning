d1666160467939ad92bfbffe07e203db1b4a59bb
Massimo Ercolani <massimo.ercolani28@gmail.com>
2018-10-15 18:46:44 +0200

    remove useless methods from user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,5 @@
 class User < ApplicationRecord
-  has_many :work_sessions, dependent: :destroy
-  has_many :work_days, dependent: :destroy
-
   validates :username, presence: true
-
-  def active_worksession
-    self.work_sessions.find_by_end_date(nil)
-  end
 
   def destroy_scheduled_jobs(job_name)
     user = self
@@ -23,32 +16,6 @@ class User < ApplicationRecord
       condition && job == job_name
     end.each(&:delete)
     job = Object.const_get job_name
-  end
-
-  def close_active_sessions
-    active_sessions = self.work_sessions.where(end_date: nil)
-    unless active_sessions.empty?
-      active_sessions.each do |session|
-        session.close
-        if session.duration < 300
-          session.delete_and_send_error
-        else
-          session.send_confirmation_message
-        end
-      end
-    end
-  end
-
-  def find_or_create_workday
-    work_day = self.work_days.find_by_date(Date.current)
-    unless work_day
-      work_day = self.work_days.create(date: Date.today)
-    end
-    work_day
-  end
-
-  def had_lunch?
-    self.find_or_create_workday.work_sessions.find_by_client("Pranzo") ? true : false
   end
 
   def is_emf?
